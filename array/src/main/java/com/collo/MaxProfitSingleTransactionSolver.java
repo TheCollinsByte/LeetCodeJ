009adeb40aac81b3761c6f11a553a46aed0885e0
colspan <collin@collindigm.com>
2024-06-30 22:26:38 +0300

    Best Time to Buy and Sell Stock class defined

---
+++ array/src/main/java/com/collo/MaxProfitSingleTransactionSolver.java
@@ -0,0 +1,17 @@
+package com.collo;
+
+/**
+ * <p>Best Time to Buy and Sell Stock (at most one transaction allowed)</p>
+ *
+ * @author Collins (TheOddagen)
+ */
+public class MaxProfitSingleTransactionSolver {
+
+    /**
+     *  Default Constructor.
+     *  Constructs a new instance of MaxProfitSingleSingleTransactionSolver
+     */
+    public MaxProfitSingleTransactionSolver() {
+    }
+
+}
---
+++ array/src/test/java/com/collo/MaxProfitSingleTransactionSolverTest.java
@@ -0,0 +1,7 @@
+package com.collo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaxProfitSingleTransactionSolverTest {
+
+}
